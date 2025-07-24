97b861cce79e39efa862e68a2454226f0d408c1a
AliAMaster <46274146+AliAMaster@users.noreply.github.com>
2025-07-24 16:04:46 +0400

    Some things

--- Custom/exercise/models/exercise.py
+++ Custom/exercise/models/exercise.py
@@ -1,9 +1,19 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class Exercise(models.Model):
-    _name = "exercise"
-    _description = "Exercise Variation"
+	_name = "exercise"
+	_description = "Exercise Variation"
 
-    name = fields.Char("Name", required=True)
-    muscle_group = fields.Many2many("muscle_group", string="Muscle Groups")
+	name = fields.Char("Name", required=True)
+	muscle_group = fields.Many2many("muscle_group", string="Muscle Groups")
+	sets = fields.One2many(comodel_name="set", inverse_name='exercise', string="Sets", compute='compute_recent_sets')
+
+	@api.depends('sets.datetime')
+	def compute_recent_sets(self):
+		for rec in self:
+			rec.sets = self.env['exercise'].search(
+				[('exercise', '=', rec.id)],
+				order='date_time dec',
+				limit=3
+			)
